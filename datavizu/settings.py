5adec44b999f86ced5525720dbc137c0e4ef8efd
FilipSmith <kill_bill_34@hotmail.com>
2018-09-20 10:58:16 +0200

    Static files issue in production

--- datavizu/settings.py
+++ datavizu/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'Berne, Suisse
+STATIC_URL = '/static/' 
 #Get path to project
 PROJECT_DIR = os.path.dirname(__file__)
 
