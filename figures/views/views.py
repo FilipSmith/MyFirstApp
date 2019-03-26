5eb3a57d53f996236e6c0f9f10c0fd7fefb5a45b
FilipSmith <kill_bill_34@hotmail.com>
2019-03-26 22:28:50 +0100

    JS to js

--- figures/views/views.py
+++ figures/views/views.py
@@ -148,8 +148,14 @@ def getData(request , doc_id):
               day_list=day_list + [varr]	
            if varr in ("TRTA",  "TRTP")    :
               trt_list=trt_list + [varr]
-			  
-       flag1 = pop_fl[1]
+       
+       try:
+           b=pop_fl.index(1)
+       except ValueError:
+              flag1=" "
+       else:
+              flag1 = pop_fl[1]
+       
        doc.n_pop_flag = flag1###df.flag1.count()
    
        dataset=mark_safe(df.to_html(classes='stripe row-border order-column').replace('&lt;','<').replace('&gt;','>').replace('<table ',' <table id="example2" style="width:100%; border-collapse: collapse; " ').replace('dataframe ',' ').replace('border="1" ','border="0" ') )
@@ -225,6 +231,82 @@ def data_dash(request, doc_id, graff  ):
 		
     return render(request, 'figures/data_dash.html', { 'document':doc ,   'graph':graph_, 'variables':variables, 'HEIGHT_VAR':HEIGHT_VAR,'WEIGHT_VAR':WEIGHT_VAR, 'BMI_VAR':BMI_VAR ,'FLAG_EFF':FLAG_EFF,'FLAG_SAF':FLAG_SAF,'WARN_MESSAGE':WARN_MESSAGE })   
 
+def dataCheck(doc_id):
+    doc=get_object_or_404(Document, id=doc_id)
+	
+    WARN_FL=""
+    WARN_VS=""
+    WARN_CDISC=""		
+
+	### NO FLag defined
+    flag_list_l = doc.pop_flag
+    if len(flag_list_l) == 0:
+       WARN_FL="There is no flag defined";  
+	
+	###A few check for unusual naming
+    HEIGHT_VAR = 'HEIGHT'
+    WEIGHT_VAR = 'WEIGHT'
+    BMI_VAR = 'BMI'
+    variables = doc.variables	
+		
+    if 'HEIGHTBL' in variables :
+        HEIGHT_VAR='HEIGHTBL'
+    if 'WEIGHTBL' in variables  :
+        WEIGHT_VAR='WEIGHTBL'
+    if 'BMIBL' in variables :
+        BMI_VAR='BMIBL'
+	
+    if HEIGHT_VAR not in variables: 
+       WARN_VS=mark_safe(WARN_VS+"<p style='color:red'> The variable " +HEIGHT_VAR+ " is missing </p>") 
+    if BMI_VAR not in variables: 
+       WARN_VS=mark_safe(WARN_VS+"<p style='color:red'> The variable " +WEIGHT_VAR+ "is missing </p>")
+    if WEIGHT_VAR not in variables: 
+       WARN_VS=mark_safe(WARN_VS+"<p style='color:red'> The variable " +BMI_VAR+ " is missing </p>")
+    if (HEIGHT_VAR not in variables) & (BMI_VAR not in variables) & (WEIGHT_VAR not in variables) :
+       WARN_VS= "<p style='color:red'> Height Weight and BMI are not available </p>" 
+
+    if doc.domain =='ADSL':
+	###CDISC ADAM
+        if  "AGE" not in variables :
+            WARN_CDISC=mark_safe(WARN_CDISC+"<p style='color:red'>AGE is required in ADSL as per CDISC rules</p>")
+			
+    res=[WARN_FL,WARN_VS,WARN_CDISC,HEIGHT_VAR,WEIGHT_VAR,BMI_VAR]
+			
+    return res
+	
+def flag_var(doc_id):
+    doc=get_object_or_404(Document, id=doc_id)
+    flag_list_l = doc.pop_flag.replace("[","").replace("]","").replace("'","").replace(' ', '')
+    flag_list_l= flag_list_l.split(',')
+
+    ###A few check for unusual naming
+    FLAG_ITTFL = 'ITTFL'
+    FLAG_SCRFL = 'SCRNFL'
+    FLAG_EFF = 'EFFL'
+    FLAG_SAF = 'SAFFL'
+    FLAG_RAND = 'RANDFL'
+    FLAG_PK = 'PKFL'
+	
+    if 'EFFICACY' in flag_list_l and 'EFFL' not in flag_list_l:
+        FLAG_EFF='EFFICACY'
+    if 'SAFETY' in flag_list_l and 'SAFFL' not in flag_list_l:
+        FLAG_SAF='SAFETY' 
+		
+		##select the more pertinent and avaialble flag  
+    if FLAG_RAND in  flag_list_l :
+       flag_list_l.insert(0, FLAG_RAND)
+    if FLAG_PK in  flag_list_l :
+       flag_list_l.insert(0, FLAG_PK)  
+    if FLAG_SCRFL in  flag_list_l :
+       flag_list_l.insert(0, FLAG_SCRFL)
+    if FLAG_EFF in  flag_list_l :
+       flag_list_l.insert(0, FLAG_EFF)		
+    if FLAG_SAF in  flag_list_l :
+       flag_list_l.insert(0, FLAG_SAF)		
+    if FLAG_ITTFL in  flag_list_l :
+       flag_list_l.insert(0, FLAG_ITTFL)		
+
+    return flag_list_l	   
  
 def data_dash2(request, doc_id  ):
     doc=get_object_or_404(Document, id=doc_id)  
@@ -233,34 +315,63 @@ def data_dash2(request, doc_id  ):
     df_raw = df    
     df = df.rename(columns=lambda x: x.upper()) 
   
-
-	
 	##########################Data for Bar Chart#############################
     df_f=df[(df.SEX == "F")].groupby(['RACE']).count()
     df_m=df[(df.SEX == "M")].groupby(['RACE']).count()
- 
- 
  
     bar_male=df_m['USUBJID'].values.tolist()
     bar_female=df_f['USUBJID'].values.tolist()
     bar_labels = mark_safe(['French', 'Chinese', 'Japanese', 'English'])
 	## bar_labels = mark_safe(df.RACE.unique().tolist())
-	
+    
+    #########################################################################
+    WARN_MESSAGE=dataCheck(doc_id)	
+    WV={}
     age_f0 = [] 
-    age_m0 = []   
+    age_m0 = []  
+    bgcolor_arm = ['rgba(0, 0, 255, 0.3)','rgba(255, 99, 132, 0.3)','rgba(0, 255, 0, 0.3)','yellow']	
+    bocolor_arm = ['rgba(0, 0, 255, 1)','rgba(255, 99, 132, 1)','rgba(0, 255, 0, 1)','yellow']	
+    if "ARMCD" not in list(df) :
+        df['ARMCD'] = df['ARM']
     armcd=df.ARMCD.unique().tolist()
-    for  val in  armcd :
+    for  num,val in  enumerate(armcd, start=1):
         age_f0.append(df[(df.SEX == "F") & (df.ARMCD == val)]['AGE'].values.tolist() )
         age_m0.append(df[(df.SEX == "M") & (df.ARMCD == val)]['AGE'].values.tolist() )
-	
+        if num == 1 :
+           df.loc[df['ARMCD'] == val, 'ARM'] = mark_safe('<span class="label label-success">' + val)
+        if num == 2 :
+           df.loc[df['ARMCD'] == val, 'ARM'] = mark_safe('<span class="label label-warning">' + val)
+        if num == 3 :
+           df.loc[df['ARMCD'] == val, 'ARM'] = mark_safe('<span class="label label-info">' + val)
+        if num == 4 :
+           df.loc[df['ARMCD'] == val, 'ARM'] = mark_safe('<span class="label label-danger">' + val)
+		
+        if 	WARN_MESSAGE[1] == '' :
+        
+            dftmp=df[(df['ARMCD'] == val)]
+            WTMP = dftmp[[WARN_MESSAGE[3],WARN_MESSAGE[4],WARN_MESSAGE[5]]].to_json(orient='records').replace('"','').replace(WARN_MESSAGE[3],"y").replace(WARN_MESSAGE[5],"r").replace(WARN_MESSAGE[4],"x")
+            WTMP_ =mark_safe( "label: ['"+ val +"'],data:" + WTMP + ",backgroundColor: '"+bgcolor_arm[num-1]+"',borderColor:  '"+bocolor_arm[num-1] +"', borderWidth: 1" ) 
+			
+            WV['line_dict_{}'.format(num)] = WTMP_
+            
+
+			
     age_f=age_f0
     age_m=age_m0
     box_labels = mark_safe(armcd)
-    #########################################################################
-	
-	 	
-	
-    return render(request, 'figures/data_dash2.html', { 'document':doc,'bar_male':bar_male, 'bar_female':bar_female, 'bar_labels':bar_labels,'box_labels':box_labels,'age_f':age_f,'age_m':age_m } )   
+    listing=df[["USUBJID", "SITEID", "ARM", "AGE"] ].values.tolist()
+
+	    
+    if 	WARN_MESSAGE[0] == '' :
+        FLAG_ = flag_var(doc_id) 
+        flag1=FLAG_[0]
+        flag2=FLAG_[1]
+        c_flag1= df_f=df[(df[flag1] == "Y")]['USUBJID'].count()	
+        c_flag2= df_f=df[(df[flag2] == "Y")]['USUBJID'].count()
+        p_flag1=100*c_flag1/df['USUBJID'].count()
+        p_flag2=100*c_flag2/df['USUBJID'].count()
+ 		
+    return render(request, 'figures/data_dash2.html', { 'document':doc,'bar_male':bar_male, 'bar_female':bar_female, 'bar_labels':bar_labels,'box_labels':box_labels,'age_f':age_f,'age_m':age_m,'listing':listing[:10] ,'WARN_MESSAGE':WARN_MESSAGE,'FLAG1':flag1,'FLAG2':flag2,'C_FLAG1':c_flag1,'C_FLAG2':c_flag2,'P_FLAG1':p_flag1,'P_FLAG2':p_flag2,'WV':WV} )   
  
     
  
